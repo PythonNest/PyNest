cad201f3c2ebb2d20decfc798a3b75862823e7d2
ItayTheDar <itda@post.bgu.ac.il>
2023-12-03 11:15:08 +0200

    create abstract class for the orm providers

--- nest/core/database/orm_provider.py
+++ nest/core/database/orm_provider.py
@@ -1,143 +1,126 @@
-from nest.core.database.orm_config import ConfigFactory, AsyncConfigFactory
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from abc import ABC, abstractmethod
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Dict, Any
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+
+from nest.core.database.orm_config import ConfigFactory, AsyncConfigFactory
 
 
 class Base(DeclarativeBase):
     """
     Base class for ORM models.
-
     """
-
     pass
 
 
-class OrmProvider:
-    """
-    Provides an interface for working with an ORM (Object-Relational Mapping) service.
-
-    Args:
-        db_type (str, optional): The type of database. Defaults to "postgresql".
-        config_params (dict, optional): Configuration parameters specific to the chosen database type.
-                                        Defaults to None.
-
-    Attributes:
-        Base: The declarative base class for defining ORM models.
-        config: The configuration factory for the chosen database type.
-        config_url: The URL generated from the database configuration parameters.
-        engine: The SQLAlchemy engine for database connection.
-
-    """
-
+class BaseOrmProvider(ABC):
     def __init__(
-        self, db_type: str = "postgresql", config_params: dict = None, **kwargs
+            self,
+            db_type: str = "postgresql",
+            config_params: dict = None,
+            async_mode: bool = False,
+            **kwargs
     ):
         """
-        Initializes the OrmService instance.
+        Initializes the BaseOrmProvider instance.
 
         Args:
-            db_type (str, optional): The type of database. Defaults to "postgresql".
-            config_params (dict, optional): Configuration parameters specific to the chosen database type.
-                                            Defaults to None.
+            db_type (str): The type of database. Defaults to "postgresql".
+            config_params (dict): Configuration parameters for the database.
+            async_mode (bool): Flag to indicate if the provider is asynchronous.
         """
-        self.Base: DeclarativeBase = Base()
-        self.config = ConfigFactory(db_type=db_type).get_config()
+        self.Base = Base
+
+        config_factory = AsyncConfigFactory if async_mode else ConfigFactory
+
+        engine_function = create_async_engine if async_mode else create_engine
+        if 'engine_params' in kwargs:
+            engine_params: Dict[str, Any] = kwargs.pop('engine_params')
+        else:
+            engine_params = {}
+
+        session_function = async_sessionmaker if async_mode else sessionmaker
+        if 'session_params' in kwargs:
+            session_params: Dict[str, Any] = kwargs.pop('session_params')
+        else:
+            session_params = {}
+
+        self.config = config_factory(db_type=db_type).get_config()
         self.config_url = self.config(**config_params).get_engine_url()
-        self.engine = create_engine(self.config_url, **kwargs)
+        self.engine = engine_function(self.config_url, **engine_params)
+        self.session = session_function(self.engine, **session_params)
+
+    @abstractmethod
+    def create_all(self):
+        pass
+
+    @abstractmethod
+    def drop_all(self):
+        pass
+
+    @abstractmethod
+    def get_db(self):
+        pass
+
+
+class OrmProvider(BaseOrmProvider):
+    """
+    Synchronous ORM provider.
+    """
+
+    def __init__(self, db_type: str = "postgresql", config_params: dict = None):
+        super().__init__(db_type=db_type, config_params=config_params)
 
     def create_all(self):
-        """
-        Creates all the tables defined by the ORM models.
-
-        Raises:
-            Exception: If there's an error creating the tables.
-        """
         self.Base.metadata.create_all(bind=self.engine)
 
     def drop_all(self):
-        """
-        Drops all the tables defined by the ORM models.
-
-        Raises:
-            Exception: If there's an error dropping the tables.
-        """
         self.Base.metadata.drop_all(bind=self.engine)
 
     def get_db(self) -> Session:
-        """
-        Returns a SQLAlchemy Session object for database operations.
-
-        Returns:
-            Session: A SQLAlchemy Session object.
-
-        Raises:
-            Exception: If there's an error creating the session.
-        """
+        db = self.session()
         try:
-            session = sessionmaker(bind=self.engine)
-            return session()
+            return db
         except Exception as e:
             raise e
+        finally:
+            db.close()
 
 
-class AsyncOrmProvider:
+class AsyncOrmProvider(BaseOrmProvider):
     """
-    Provides ORM services for asynchronous database operations.
-
-    Attributes:
-        Base (DeclarativeBase): The base class for declarative class definitions.
-        config (function): Configuration function for the database.
-        config_url (str): Database connection URL.
-        engine: Asynchronous engine for the database.
-        Session: Asynchronous sessionmaker for creating sessions.
+    Asynchronous ORM provider.
     """
 
-    def __init__(
-        self, db_type: str = "postgresql", config_params: dict = None, **kwargs
-    ):
-        """
-        Initializes the AsyncOrmProvider with specified database configuration.
-
-        Args:
-            db_type (str): Type of database (e.g., 'postgresql').
-            config_params (dict): Configuration parameters for the database.
-            **kwargs: Additional keyword arguments.
-        """
-        self.Base: DeclarativeBase = Base
-        self.config = AsyncConfigFactory(db_type=db_type).get_config()
-        self.config_url = self.config(**config_params).get_engine_url()
-        self.engine = create_async_engine(self.config_url, echo=True, **kwargs)
-        self.Session = async_sessionmaker(
-            self.engine, expire_on_commit=False, class_=AsyncSession
+    def __init__(self, db_type: str = "postgresql", config_params: dict = None, **kwargs):
+        kwargs['engine_params'] = dict(echo=True)
+        kwargs['session_params'] = dict(
+            expire_on_commit=False,
+            class_=AsyncSession
         )
+        super().__init__(db_type=db_type, config_params=config_params, async_mode=True, **kwargs)
 
     async def create_all(self):
-        """
-        Creates all tables in the database asynchronously.
-        """
         async with self.engine.begin() as conn:
             await conn.run_sync(self.Base.metadata.create_all)
 
     async def drop_all(self):
-        """
-        Drops all tables in the database asynchronously.
-        """
         async with self.engine.begin() as conn:
             await conn.run_sync(self.Base.metadata.drop_all)
 
     async def get_db(self) -> AsyncSession:
-        db = self.Session()
+        db = self.session()
         try:
             yield db
         finally:
             await db.close()
 
     @asynccontextmanager
-    async def get_self_db(self) -> AsyncGenerator[AsyncSession, None]:
-        db = self.Session()
+    async def session(self) -> AsyncSession:
+        db = self.session()
         try:
             yield db
         finally:
