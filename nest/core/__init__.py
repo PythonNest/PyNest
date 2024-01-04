ded84e27350d3fc95634079a61cb89ec7249ceda
ItayTheDar <itda@post.bgu.ac.il>
2024-01-04 14:53:02 +0200

    fix bad import

--- nest/core/__init__.py
+++ nest/core/__init__.py
@@ -1,4 +1,3 @@
-from nest.core.database.base_orm import OrmService
 from nest.core.decorators import Get, Post, Put, Delete, Patch, Controller, Module
 from fastapi import Depends
-from nest.core.pynest_factory import PynestFactory
+from nest.core.pynest_factory import PyNestFactory
