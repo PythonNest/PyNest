e6ddfef57765b7b52d2fa9588aba8ca828b71c4e
ItayTheDar <itda@post.bgu.ac.il>
2024-01-05 08:38:44 +0200

    modify blank app example to work with module decorator

--- examples/BlankApp/src/example/example_module.py
+++ examples/BlankApp/src/example/example_module.py
@@ -1,10 +1,12 @@
 from .example_controller import ExampleController
 from .example_service import ExampleService
+from nest.core import Module
 
 
+@Module(
+    controllers=[ExampleController],
+    providers=[ExampleService],
+    imports=[]
+)
 class ExampleModule:
-
-    def __init__(self):
-        self.controllers = [ExampleController]
-        self.providers = [ExampleService]
-
+    pass
--- examples/BlankApp/src/example/example_service.py
+++ examples/BlankApp/src/example/example_service.py
@@ -1,17 +1,18 @@
 from .example_model import Example
 from functools import lru_cache
+from nest.core.decorators import Injectable
 
 
 @lru_cache()
+@Injectable
 class ExampleService:
 
     def __init__(self):
         self.database = []
-        
+
     def get_example(self):
         return self.database
-    
+
     def add_example(self, example: Example):
         self.database.append(example)
         return example
-        
