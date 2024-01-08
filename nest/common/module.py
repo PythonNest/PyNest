118f075342d3c4781e89e1054a2c344403c63687
Daniel Moumeny <107214389+danielmoumeny212@users.noreply.github.com>
2024-01-09 00:38:27 +0100

    fixing PyNestContainer) -> Module circular import

--- nest/common/module.py
+++ nest/common/module.py
@@ -5,7 +5,6 @@ import string
 from typing import List
 from uuid import uuid4
 from typing import Any, Type
-from nest.core import PyNestContainer 
 
 class ModulesContainer(dict):
     def __init__(self):
@@ -26,7 +25,7 @@ class ModulesContainer(dict):
 
 
 class Module:
-    def __init__(self, metatype: Type[object], container: PyNestContainer):
+    def __init__(self, metatype: Type[object], container):
         self._id = str(uuid.uuid4())
         self._metatype = metatype
         self.container = container
