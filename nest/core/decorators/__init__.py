65111769260a0b05ccddbe6bc771a3cd0fd31a01
ItayTheDar <itda@post.bgu.ac.il>
2024-03-06 09:08:42 +0200

    move http methods decorators to seperate file

--- nest/core/decorators/__init__.py
+++ nest/core/decorators/__init__.py
@@ -1,2 +1,3 @@
-from nest.core.decorators.controller import Controller, Get, Post, Delete, Put, Patch
+from nest.core.decorators.controller import Controller
+from nest.core.decorators.http_methods import Get, Post, Delete, Put, Patch
 from nest.core.decorators.database import db_request_handler, async_db_request_handler
