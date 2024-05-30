da38148f2cc113d3704b07f107ce16d3db3bd1ee
Amir Lavasani <amirm.lavasani@gmail.com>
2024-05-30 09:05:51 +0330

    refactor: HTTPCode decorator
    move HTTPCode decorator from utils into its own module file.

--- nest/core/decorators/__init__.py
+++ nest/core/decorators/__init__.py
@@ -1,5 +1,5 @@
 from nest.core.decorators.controller import Controller
 from nest.core.decorators.http_method import Delete, Get, Patch, Post, Put
+from nest.core.decorators.http_code import HttpCode
 from nest.core.decorators.injectable import Injectable
 from nest.core.decorators.module import Module
-from nest.core.decorators.utils import HttpCode
--- nest/core/decorators/utils.py
+++ nest/core/decorators/utils.py
@@ -1,23 +1,7 @@
 import ast
 import inspect
 
-from nest.common.constants import INJECTABLE_TOKEN, STATUS_CODE_TOKEN
-
-
-def HttpCode(status_code: int):
-    """
-    Decorator that sets the HTTP status code for a route.
-
-    Args:
-        status_code (int): The HTTP status code for the response.
-    """
-
-    def decorator(func):
-        if not hasattr(func, STATUS_CODE_TOKEN):
-            setattr(func, STATUS_CODE_TOKEN, status_code)
-        return func
-
-    return decorator
+from nest.common.constants import INJECTABLE_TOKEN
 
 
 def get_instance_variables(cls):
