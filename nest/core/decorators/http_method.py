6881ae6544e39cbe86782e5bc8238ec4309d1a6b
ItayTheDar <itda@post.bgu.ac.il>
2024-06-21 13:28:54 +0300

    modify http function so there signature will accept kwargs

--- nest/core/decorators/http_method.py
+++ nest/core/decorators/http_method.py
@@ -35,27 +35,29 @@ def route(http_method: HTTPMethod, route_path: Union[str, List[str]] = "/", **kw
     return decorator
 
 
-# Decorator for defining a GET route with an optional path
-Get: Callable[[Union[str, List[str]]], Callable[..., Any]] = (
-    lambda route_path="/", **kwargs: route(HTTPMethod.GET, route_path, **kwargs)
-)
+def Get(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.GET, route_path, **kwargs)
 
-# Decorator for defining a POST route with an optional path
-Post: Callable[[Union[str, List[str]]], Callable[..., Any]] = (
-    lambda route_path="/", **kwargs: route(HTTPMethod.POST, route_path, **kwargs)
-)
 
-# Decorator for defining a DELETE route with an optional path
-Delete: Callable[[Union[str, List[str]]], Callable[..., Any]] = (
-    lambda route_path="/", **kwargs: route(HTTPMethod.DELETE, route_path, **kwargs)
-)
+def Post(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.POST, route_path, **kwargs)
 
-# Decorator for defining a PUT route with an optional path
-Put: Callable[[Union[str, List[str]]], Callable[..., Any]] = (
-    lambda route_path="/", **kwargs: route(HTTPMethod.PUT, route_path, **kwargs)
-)
 
-# Decorator for defining a PATCH route with an optional path
-Patch: Callable[[Union[str, List[str]]], Callable[..., Any]] = (
-    lambda route_path="/", **kwargs: route(HTTPMethod.PATCH, route_path, **kwargs)
-)
+def Delete(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.DELETE, route_path, **kwargs)
+
+
+def Put(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.PUT, route_path, **kwargs)
+
+
+def Patch(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.PATCH, route_path, **kwargs)
+
+
+def Head(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.HEAD, route_path, **kwargs)
+
+
+def Options(route_path: Union[str, List[str]] = "/", **kwargs) -> Callable[..., Any]:
+    return route(HTTPMethod.OPTIONS, route_path, **kwargs)
