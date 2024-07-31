1df4f431234d531285b3922a9bd1973c6ad6ec21
itay.dar <itay.dar@lemonade.com>
2024-07-31 20:38:09 +0300

    add support in async cli commands

--- nest/core/cli_factory.py
+++ nest/core/cli_factory.py
@@ -1,5 +1,5 @@
 import click
-
+import asyncio
 from nest.core.pynest_factory import AbstractPyNestFactory, ModuleType
 from nest.core.pynest_container import PyNestContainer
 
@@ -13,8 +13,18 @@ class CLIAppFactory(AbstractPyNestFactory):
         container.add_module(app_module)
 
         cli_app = click.Group("main")
-
         for module in container.modules.values():
             for controller in module.controllers.values():
+                for command in controller._cli_group.commands.values():
+                    original_callback = command.callback
+                    if asyncio.iscoroutinefunction(original_callback):
+                        command.callback = self._run_async(original_callback)
                 cli_app.add_command(controller._cli_group)
         return cli_app
+
+    @staticmethod
+    def _run_async(coro):
+        def wrapper(*args, **kwargs):
+            return asyncio.run(coro(*args, **kwargs))
+        return wrapper
+
