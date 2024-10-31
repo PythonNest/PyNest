517f6a7a89f545e3a4394fd043a68c7c6355e9d4
itay.dar <itay.dar@lemonade.com>
2024-10-31 14:44:58 +0200

    remove unused files

--- nest/plugins/modules/redis/redis_module.py
+++
@@ -1,8 +0,0 @@
-from nest.core import Module
-from nest.plugins.modules.redis.redis_controller import RedisController
-from nest.plugins.modules.redis.redis_service import RedisService
-
-
-@Module(controllers=[RedisController], providers=[RedisService], imports=[])
-class RedisModule:
-    pass
