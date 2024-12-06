cd45764bdbcb948a201f355eecefa289f7982678
itay.dar <itay.dar@lemonade.com>
2024-12-06 08:44:10 +0200

    make odm provider support in kwargs

--- nest/core/database/odm_provider.py
+++ nest/core/database/odm_provider.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Type
 
 from beanie import Document, init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -14,7 +14,7 @@ class OdmProvider:
         db_type (str, optional): The type of database. Defaults to "mongodb".
         config_params (dict, optional): Configuration parameters specific to the chosen database type.
                                         Defaults to None.
-        document_models (beanie.Document): a list of beanie.Document instances
+        document_models (List[Type[Document]]): A list of beanie.Document subclasses.
 
     Attributes:
         config: The configuration factory for the chosen database type.
@@ -26,34 +26,35 @@ class OdmProvider:
         self,
         db_type="mongodb",
         config_params: dict = None,
-        document_models: List[Document] = None,
+        document_models: list[Type[Document]] = None,
     ):
         """
-        Initializes the OrmService instance.
+        Initializes the OdmProvider instance.
 
         Args:
             db_type (str, optional): The type of database. Defaults to "mongodb".
             config_params (dict, optional): Configuration parameters specific to the chosen database type.
                                             Defaults to None.
-            document_models (beanie.Document): a list of beanie.Document instances
+            document_models (List[Type[Document]]): A list of beanie.Document subclasses.
         """
-
         self.config_object = ConfigFactory(db_type=db_type).get_config()
         self.config = self.config_object(**config_params)
         self.config_url = self.config.get_engine_url()
-        self.document_models = document_models
+        self.document_models = document_models or []
 
     async def create_all(self):
+        """
+        Initializes the Beanie ODM with the provided document models.
+        """
         self.check_document_models()
         client = AsyncIOMotorClient(self.config_url)
         await init_beanie(
-            database=client[self.config.db_name], document_models=self.document_models
+            database=client.get_default_database(), document_models=self.document_models
         )
 
     def check_document_models(self):
         """
-        Checks that the document_models argument is a list of beanie.Document instances.
-
+        Checks that the document_models argument is a list of beanie.Document subclasses.
         """
         if not isinstance(self.document_models, list):
             raise Exception("document_models should be a list")
